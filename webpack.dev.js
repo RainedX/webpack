364baaa61e50e4e950dfe100cd16a53e8f6a4e43
yanrui <yanrui@weli.cn>
2019-07-04 18:57:46 +0800

    8880

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
     devServer: {
         contentBase: './dist',
-        port: '8888',
+        port: '8880',
         // inline: true,
         historyApiFallback: true,
         hot: true
