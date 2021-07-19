d902947fdba78c2835d72a072bbb5b8eb2108fe2
RainedX <1355247497@qq.com>
2021-07-19 10:07:46 +0800

    feat：sourcemap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import './index.css';
-import './index.less';
-import './avatar.png';
+// let logo =  require('./avatar.png');
 
-const moduleA = require('./moduleA.js');
+// let image = new Image();
+// image.src = logo;
+// document.body.appendChild(image);
+
+let sum = (a, b) => {
+  console.log(1111, v);
+  return a - b;
+} 
+
+sum();
+
+// const p = new Promise((resolve, reject) => {
+//   resolve(1);
+// });
+
+// const moduleA = require('./moduleA.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FileManagerWebpackPlugin = require("filemanager-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
+  devtool: 'source-map',
   entry: {
     index: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    filename: "js/[name].[hash:8].js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/"
   },
   module: {
     rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  // {
+                  //   useBuiltIns: 'usage', // 按需要加载polyfill
+                  //   corejs: {
+                  //     version: 3, // 指定core-js版本
+                  //   },
+                  // }
+                ],
+                "@babel/preset-react"
+              ]
+            }
+          }
+        ]
+      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
@@ -21,14 +46,42 @@ module.exports = {
       {
         test: /\.less$/,
         use: ['style-loader', 'css-loader', 'less-loader']
-      }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            esModule: false,
+            name: 'avatar.[ext]',
+            limit: 8 * 1024
+          }
+        }]
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/template.html"),
     }),
-    new webpack.HotModuleReplacementPlugin({})
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*']
+    }),
+    // new webpack.SourceMapDevToolPlugin({
+    //   filename: '[file].map',
+    //   append: '\n//# sourceMappingURL=http://localhost:3000/[url]',
+    // }),
+    // new FileManagerWebpackPlugin({
+    //   events: {
+    //     onEnd: {
+    //       copy: [{
+    //         source: './dist/*.map',
+    //         destination: path.resolve(__dirname, 'sourcemap')
+    //       }],
+    //       delete: ['./dist/*.map']
+    //     }
+    //   }
+    // })
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
